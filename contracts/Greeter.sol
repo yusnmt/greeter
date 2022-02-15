145c2f23502188fb396db1e42901a559e3b08b36
yusnmt <yusnmt@gmail.com>
2022-02-15 12:08:21 +0900

    first commit

---
+++ contracts/Greeter.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.8.11;
+
+contract Greeter {
+    string private _greeting = "Hello, World!";
+
+    function greet() external view returns(string memory) {
+        return _greeting;
+    }
+
+    function setGreeting(string calldata greeting) external {
+        _greeting = greeting;
+    }
+}
